3807db0e3f81ac6019bbdfb5abb91d8e2c41be6f
pallobbiswas <pallobbiswas326@gmail.com>
2022-05-30 12:13:12 +0600

    brand added

---
+++ src/components/Home/Brand.js
@@ -0,0 +1,34 @@
+import React from "react";
+import airban from "../../images/logos/airbnb.png";
+import google from "../../images/logos/google.png";
+import netflix from "../../images/logos/netflix.png";
+import slack from "../../images/logos/slack.png";
+import uber from "../../images/logos/uber.png";
+
+const Brand = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="mx-20">
+        <div className="grid gris-cols-1 md:grid-cols-5 my-10">
+          <div>
+            <img style={{width: '129px', height:'44px'}} src={slack} alt="" />
+          </div>
+          <div>
+            <img style={{width: '129px', height:'44px'}} src={google} alt="" />
+          </div>
+          <div>
+            <img style={{width: '129px', height:'44px'}} src={uber} alt="" />
+          </div>
+          <div>
+            <img style={{width: '129px', height:'44px'}} src={netflix} alt="" />
+          </div>
+          <div>
+            <img style={{width: '129px', height:'44px'}} src={airban} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Brand;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Brand from './Brand';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Brand />
         </div>
     );
 };
