f585417e353c7ac85ba422430581c7ba4b2eab19
pallobbiswas <pallobbiswas326@gmail.com>
2022-05-31 18:20:48 +0600

    add reviw done

--- src/components/Dashboard/Addproducts.js
+++ src/components/Dashboard/Addproducts.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Addproducts = () => {
-    return (
-        <div>
-            <h1>products</h1>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div>
+      <div className="ml-14">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="name"
+            className="input input-bordered mt-6 rounded-lg w-full"
+            {...register("name")}
+          />
+          <br />
+          <input
+            placeholder="message"
+            className="input input-bordered mt-6 rounded-lg w-full h-44"
+            {...register("message")}
+          />
+          <br />
+          <input
+            className="input input-bordered mt-6 btn btn-primary rounded-lg px-10"
+            type="submit"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Addproducts;
