3e710c6311720e3b97fd6e01ee69bdb63d42155e
pallobbiswas <pallobbiswas326@gmail.com>
2022-06-02 12:35:20 +0600

    cliping added

--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 const useAdmin = (user) => {
 
     const[admin, setAdmin] = useState(true);
-    const[adminLoading, setAdminLoading] = useState(false);
 
     useEffect(()=>{
         fetch(`http://localhost:5000/users/${user?.email}`)
